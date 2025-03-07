8edf27b0f81e36b07e11c03f46e98c534bbebbe7
athenova <aleksey.sergeyev@yandex.com>
2025-03-07 21:16:00 +0300

    Удалил расписание

--- send.py
+++ send.py
@@ -1,14 +1,5 @@
 from project import Project
-import schedule
-import time
 
 if __name__ == '__main__':
     p = Project()
-
-    schedule.every().day.at("08:00",'Europe/Moscow').do(p.send)
-
-    hour = 60 * 60
-
-    for i in range(hour):
-        schedule.run_pending()
-        time.sleep(1)
+    p.send()
